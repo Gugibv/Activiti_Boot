9d5aa09dacfa89bdaefc269a4c274975ec17ef9a
gugibv <gugibv@163.com>
2022-03-03 00:10:12 +0800

    更新activiti测试类

--- activiti-boot/src/main/java/com/grey/workflow/config/SecurityConfig.java
+++ activiti-boot/src/main/java/com/grey/workflow/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.grey.workflow;
+package com.grey.workflow.config;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
